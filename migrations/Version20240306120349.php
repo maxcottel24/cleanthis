e0d668c79608852a4173efc813a1e314d3dd03b9
Nacim <nacim.ouldrabah@gmail.com>
2024-03-07 09:08:05 +0100

    recupération des migrations

--- migrations/Version20240306120349.php
+++ migrations/Version20240306120349.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240306083621 extends AbstractMigration
+final class Version20240306120349 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,16 +20,12 @@ final class Version20240306083621 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE invitation (id INT AUTO_INCREMENT NOT NULL, employee_id INT DEFAULT NULL, email VARCHAR(255) NOT NULL, uuid BINARY(16) NOT NULL COMMENT \'(DC2Type:uuid)\', UNIQUE INDEX UNIQ_F11D61A28C03F15C (employee_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE invitation ADD CONSTRAINT FK_F11D61A28C03F15C FOREIGN KEY (employee_id) REFERENCES users (id)');
         $this->addSql('ALTER TABLE users ADD reset_token VARCHAR(100) DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE invitation DROP FOREIGN KEY FK_F11D61A28C03F15C');
-        $this->addSql('DROP TABLE invitation');
         $this->addSql('ALTER TABLE users DROP reset_token');
     }
 }
