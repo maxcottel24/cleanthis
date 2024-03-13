c056ac04b14f5e2df6a683628cc3425d5d728029
OnaFlorent <bflorent53170@gmail.com>
2024-03-13 19:17:09 +0100

    ResetPasswordFront

--- src/Form/ResetPasswordType.php
+++ src/Form/ResetPasswordType.php
@@ -6,18 +6,30 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
 class ResetPasswordType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('password', PasswordType::class, [
-                'label' => 'Entrez votre nouveau mot de passe',
-                'attr' => [
-                    'class' => 'form-control'
-                ]
-            ])
+            ->add('password' , RepeatedType::class , [
+
+                'type' => PasswordType::class ,
+                'first_options' => [
+                   'label' => 'Entrez votre nouveau mot de passe *',
+                   'attr' => [
+                       'class'=> 'form-control'
+                   ]
+                ],
+                'second_options' => [
+                   'label' =>'Confirmation du mot de passe *',
+                   'attr' => [
+                       'class'=> 'form-control'
+                   ] 
+                ],
+                'invalid_message' => 'Les mots de passe ne correspondent pas.'
+               ])
         ;
     }
 
