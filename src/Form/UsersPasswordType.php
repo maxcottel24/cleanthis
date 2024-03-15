4130afafbfc853fe69ef22a9e8123e11fd4a4216
OnaFlorent <bflorent53170@gmail.com>
2024-03-15 13:57:09 +0100

    fix/EditProfilePassword

--- src/Form/UsersPasswordType.php
+++ src/Form/UsersPasswordType.php
@@ -6,49 +6,50 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\FormBuilderInterface ;
+use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
-class UsersPasswordType extends AbstractType {
-    public function buildForm(FormBuilderInterface  $builder, array $options): void
+class UsersPasswordType extends AbstractType
 {
-    $builder 
-    ->add('password' , RepeatedType::class , [
+    public function buildForm(FormBuilderInterface  $builder, array $options): void
+    {
+        $builder
+            ->add('password', RepeatedType::class, [
 
-        'type' => PasswordType::class ,
-        'first_options' => [
-           'label' => 'Mot de passe : 8 caractère minimum',
-           'label_attr' => ['class' => 'form-label'],
-           'attr' => [
-               'class'=> 'form-control', 
-               'placeholder' => '1 Majuscule, 1 minuscule, 1chiffre, 1caractère spécial'
-           ]
-        ],
-        'second_options' => [
-           'label' =>'Confirmation du mot de passe : ',
-           'label_attr' => ['class' => 'form-label'],
-           'attr' => [
-               'class'=> 'form-control'
-           ] 
-        ],
-        'invalid_message' => 'Les mots de passe ne correspondent pas.'
-       ])
+                'type' => PasswordType::class,
+                'first_options' => [
+                    'label' => 'Mot de passe : (8 caractères minimum)',
+                    'label_attr' => ['class' => 'form-label'],
+                    'attr' => [
+                        'class' => 'form-control',
+                        'placeholder' => '1 maj, 1 min, 1 chiffre, 1 caractère spécial'
+                    ]
+                ],
+                'second_options' => [
+                    'label' => 'Confirmation du mot de passe : ',
+                    'label_attr' => ['class' => 'form-label'],
+                    'attr' => [
+                        'class' => 'form-control'
+                    ]
+                ],
+                'invalid_message' => 'Les mots de passe ne correspondent pas.'
+            ])
 
-       ->add('newPassword' , PasswordType::class , [
-        'attr' => ['class' => 'form-control'],
-        'label' => 'Nouveau mot de passe : ' ,
-         'label_attr' => ['class' => 'form-label'],
-         'constraints' =>  [ new NotBlank(),
-         ]
-       ]) 
+            ->add('newPassword', PasswordType::class, [
+                'attr' => ['class' => 'form-control'],
+                'label' => 'Nouveau mot de passe : ',
+                'label_attr' => ['class' => 'form-label'],
+                'constraints' =>  [
+                    new NotBlank(),
+                ]
+            ])
 
-        ->add('submit', SubmitType::class, [
-            'attr' => [
-                'class' => 'btn btn-primary mt-4', 
-                'id' => 'btn-confirm'
-            ],
-            'label' => 'Confirmer' ,
-        ])
-        ;
+            ->add('submit', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-primary mt-4',
+                    'id' => 'btn-confirm'
+                ],
+                'label' => 'Confirmer',
+            ]);
     }
 }
